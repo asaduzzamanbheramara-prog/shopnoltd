e389af424a3382195907797fe83d1a101c5dd147
asaduzzamanbheramara-prog <asaduzzaman.bheramara@gmail.com>
2025-10-23 01:05:04 +0600

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,18 +1,25 @@
 <?php
+
 return [
+
+    // Default database connection
     'default' => env('DB_CONNECTION', 'pgsql'),
+
     'connections' => [
+
         'pgsql' => [
             'driver'   => 'pgsql',
-            'host'     => env('DB_HOST','db'),
-            'port'     => env('DB_PORT','5432'),
-            'database' => env('DB_DATABASE','shopnoltd'),
-            'username' => env('DB_USERNAME','shopuser'),
-            'password' => env('DB_PASSWORD','Asad18081978#'),
+            'host'     => env('DB_HOST', 'db'),
+            'port'     => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'shopnoltd'),
+            'username' => env('DB_USERNAME', 'shopuser'),
+            'password' => env('DB_PASSWORD', 'Asad18081978#'),
             'charset'  => 'utf8',
             'prefix'   => '',
             'schema'   => 'public',
             'sslmode'  => 'prefer',
         ],
+
     ],
+
 ];
