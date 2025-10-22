6cc14677d3fd128882ed0827abbaf9e5c05dd131
asaduzzamanbheramara-prog <asaduzzaman.bheramara@gmail.com>
2025-10-23 01:54:19 +0600

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,25 +1,30 @@
 <?php
 
 return [
-
-    // Default database connection
     'default' => env('DB_CONNECTION', 'pgsql'),
 
     'connections' => [
-
-        'pgsql' => [
-            'driver'   => 'pgsql',
-            'host'     => env('DB_HOST', 'db'),
-            'port'     => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'shopnoltd'),
-            'username' => env('DB_USERNAME', 'shopuser'),
-            'password' => env('DB_PASSWORD', 'Asad18081978#'),
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-            'sslmode'  => 'prefer',
+        'mysql' => [
+            'driver' => 'mysql',
+            'host' => env('DB_HOST','127.0.0.1'),
+            'database' => env('DB_DATABASE','superapp_db'),
+            'username' => env('DB_USERNAME','root'),
+            'password' => env('DB_PASSWORD',''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
         ],
 
+        'pgsql' => [
+            'driver' => 'pgsql',
+            'host' => env('DB_HOST','db'),
+            'port' => env('DB_PORT','5432'),
+            'database' => env('DB_DATABASE','shopnoltd_db'),
+            'username' => env('DB_USERNAME','shopnouser'),
+            'password' => env('DB_PASSWORD','Asad18081978#'),
+            'charset' => 'utf8',
+            'prefix' => '',
+            'schema' => 'public',
+            'sslmode' => 'prefer',
+        ],
     ],
-
 ];
