5988ee071a318b6228c63634873c0a0a174a6e17
asaduzzamanbheramara-prog <asaduzzaman.bheramara@gmail.com>
2025-10-23 02:15:54 +0600

    Update database.php

--- backend/config/database.php
+++ backend/config/database.php
@@ -1,30 +1,74 @@
 <?php
 
 return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Database Connection Name
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify which of the database connections below you wish
+    | to use as your default connection for all database work. You may use
+    | multiple connections using the Database library.
+    |
+    */
+
     'default' => env('DB_CONNECTION', 'pgsql'),
 
+    /*
+    |--------------------------------------------------------------------------
+    | Database Connections
+    |--------------------------------------------------------------------------
+    |
+    | Here are each of the database connections setup for your application.
+    | Examples of configuring both MySQL and PostgreSQL are provided below.
+    |
+    */
+
     'connections' => [
+
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST','127.0.0.1'),
-            'database' => env('DB_DATABASE','superapp_db'),
-            'username' => env('DB_USERNAME','root'),
-            'password' => env('DB_PASSWORD',''),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'superapp_db'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
         ],
 
         'pgsql' => [
-            'driver' => 'pgsql',
-            'host' => env('DB_HOST','db'),
-            'port' => env('DB_PORT','5432'),
-            'database' => env('DB_DATABASE','shopnoltd'),
-            'username' => env('DB_USERNAME','shopuser'),
-            'password' => env('DB_PASSWORD','Asad18081978#'),
-            'charset' => 'utf8',
-            'prefix' => '',
-            'schema' => 'public',
-            'sslmode' => 'prefer',
+            'driver'   => 'pgsql',
+            'host'     => env('DB_HOST', 'db'),
+            'port'     => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'shopnoltd_db'),
+            'username' => env('DB_USERNAME', 'shopnouser'),
+            'password' => env('DB_PASSWORD', 'Asad18081978#'),
+            'charset'  => 'utf8',
+            'prefix'   => '',
+            'schema'   => 'public',
+            'sslmode'  => 'prefer',
         ],
+
     ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Migration Repository Table
+    |--------------------------------------------------------------------------
+    |
+    | This table keeps track of all the migrations that have already run for
+    | your application. Using this information, we can determine which of
+    | the migrations on disk haven't actually been run in the database.
+    |
+    */
+
+    'migrations' => 'migrations',
+
 ];
